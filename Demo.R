d699b0b219ab2290a1cf530900e3de53a59f4a10
Gallinator <lcgallinator4@gmail.com>
2023-12-29 09:34:20 +0100

    Reformat code

--- Demo.R
+++ Demo.R
@@ -1,13 +1,13 @@
 library(fsdaR)
 
 
-data <- iris[,3:4]
+data <- iris[, 3:4]
 summary(data)
 cov(data)
-data<-scale(data)
+data <- scale(data)
 plot(data)
 
-?tclustIC
-out <- tclustIC(data,plot=TRUE,whichIC = 'MIXMIX')
-?tclustICsol
-solutions <- tclustICsol(out,NumberOfBestSolutions = 3,plot=TRUE,whichIC = 'MIXMIX',Rand = TRUE)
+? tclustIC
+out <- tclustIC(data, plot = TRUE, whichIC = 'MIXMIX')
+? tclustICsol
+solutions <- tclustICsol(out, NumberOfBestSolutions = 3, plot = TRUE, whichIC = 'MIXMIX', Rand = TRUE )
